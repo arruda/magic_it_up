f634089015b3d17171479334fc61d353b550252c
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2017-03-11 22:56:50 -0300

    changes in simple udp test

--- jagger/jagger/simpler_udp.py
+++ jagger/jagger/simpler_udp.py
@@ -15,19 +15,15 @@ port = 8765
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 orig = (host, port)
 udp.bind(orig)
-# initialize_keyboard()
 try:
 
     while True:
         client_msg_binary, client = udp.recvfrom(1024)
+        now = datetime.datetime.utcnow()
         client_msg = client_msg_binary.decode('UTF-8')
-
         client_time, msg = client_msg.split('>>>:')
 
         client_time = dateutil.parser.parse(client_time)
-
-        now = datetime.datetime.utcnow()
-        client_time, msg = client_msg
         time_delay = now - client_time
         # time delay in miliseconds
         time_delay = time_delay.total_seconds() * 1000
